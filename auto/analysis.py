49d3aa76fab197f4418661b180512f6e822cb964
Yin Deming <yindeming1@gmail.com>
2013-10-25 15:58:11 +0200

    update

---
+++ auto/analysis.py
@@ -0,0 +1,17 @@
+import os, sys, time
+import hashlib
+from commands import getoutput 
+import shutil, subprocess
+import glob
+
+# -----------------------------------------------------------------------
+
+# on linux this is:
+# tshark = '/usr/bin/tshark' 
+# inetsim = '/data/inetsim/inetsim'
+# python =  '/usr/bin/python'
+# volatility = '/auto/volatility/volatility'
+nvcc = 'C:\\nse7legacy\\nvcc.exe'
+
+# -----------------------------------------------------------------------
+
