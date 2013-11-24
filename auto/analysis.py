def3cb6dfb5f5a52637238c63e31476d0bce085e
Yin Deming <yindeming1@gmail.com>
2013-11-24 15:54:31 +0100

    Update analysis.py

--- auto/analysis.py
+++ auto/analysis.py
@@ -1,17 +1,34 @@
+#!/usr/bin/python 
+from vmauto import VMwareAuto
 import os, sys, time
 import hashlib
-from commands import getoutput 
-import shutil, subprocess
-import glob
+import subprocess
 
-# -----------------------------------------------------------------------
+# name of the clean snapshot
+snapname = 'clean'
 
-# on linux this is:
-# tshark = '/usr/bin/tshark' 
-# inetsim = '/data/inetsim/inetsim'
-# python =  '/usr/bin/python'
-# volatility = '/auto/volatility/volatility'
-nvcc = 'C:\\nse7legacy\\nvcc.exe'
+# path to vmware guest's VMX configuration file
+vmx = 'vmware/WindowsXP/WindowsXP.vmx'
 
-# -----------------------------------------------------------------------
+def analyze(vm, sample):
+    # copy the malware sample to the VM's hard  drive
+    dst = 'C:\\%s' % os.path.basename(sample)
+    vm.copytovm(sample, dst)
 
+    # use the script to execute the sample in the guest VM, let it run
+    # for 2 minutes
+    vm.winexec('C:\\auto.exe')
+    time.sleep(120)
+
+    # shut down the VM
+    vm.stop()
+
+    # mount the disk and scan
+    
+
+    # revert VM to clean snapshot
+    vm.revert(clean)
+    vm.start()
+
+    
+def main():
