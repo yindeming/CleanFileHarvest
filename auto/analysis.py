b9346555ed1070593769fb519beb029e36c637d2
Yin Deming <yindeming1@gmail.com>
2013-11-27 22:34:27 +0100

    Update analysis.py

--- auto/analysis.py
+++ auto/analysis.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python 
 from vmauto import VMwareAuto
 import os, sys, time
-import hashlib
 import subprocess
 
 # name of the clean snapshot
 snapname = 'clean'
 
 # path to vmware guest's VMX configuration file
-vmx = 'vmware/WindowsXP/WindowsXP.vmx'
+vmx = 'data/WindowsXP/WindowsXP.vmx'
+disk = 'data/WindowsXP/WindowxXP.vmdk'
+nvcc = 'data/scanner/nvcc.exe'
 
 def analyze(vm, sample):
     # copy the malware sample to the VM's hard  drive
@@ -17,14 +18,23 @@ def analyze(vm, sample):
 
     # use the script to execute the sample in the guest VM, let it run
     # for 2 minutes
-    vm.winexec('C:\\auto.exe')
-    time.sleep(120)
+    vm.winexec('C:\\auto_install.exe')
+    time.sleep(300)
 
     # shut down the VM
     vm.stop()
 
-    # mount the disk and scan
+    # mount the disk image
+    mount = subprocess.Popen(vmware-mount, disk, '-o', 'ro', 'mount_tmp')
     
+    # scan
+    scan = subprocess.Popen(wine, nvcc, '/C:0', '/S:0', '/U', '/L:2', 'D:')
+    
+    # wait for scanning to finish
+    time.sleep(1200)
+    
+    # unmount the disk image
+    unmount = subprocess.Popen(vmware-mount, '-x')
 
     # revert VM to clean snapshot
     vm.revert(clean)
